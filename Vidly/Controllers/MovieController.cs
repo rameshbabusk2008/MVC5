a907994e8aa04e6250ec33304faa2a85b135d55e
rameshbabusk2008 <rameshbabusk2008@gmail.com>
2020-04-21 22:10:21 +0530

    added movie control and Partial view

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie()
+            {
+                Id = 1,
+                Name = "Crown"
+
+            };
+
+            return View(movie);
+        }
+    }
+}
